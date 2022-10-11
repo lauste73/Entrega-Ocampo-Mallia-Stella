ad8e0194a3dc0fce5ab295f50f69b7e78979b20d
Leandro <leanma0011@gmail.com>
2022-10-10 22:49:45 -0300

    modelo creado

--- home1/models.py
+++ home1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    edad=models.IntegerField()
+    fecha_creacion=models.DateField(null=True)
+    
+    
+    def __str__(self):
+        return f'{self.nombre} {self.apellido} {self.edad} {self.fecha_creacion}'
