567c0df160e07f7ceaa8e2cbd399220c10102abe
Lautaro Stella <lauta.stella94@gmail.com>
2022-10-12 18:57:14 -0300

    Agregue un modelo para el futuro Chat

--- home1/urls.py
+++ home1/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from home1 import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('crear-usuario/', views.crear_usuario, name='crear_usuario'),
     path('ver-usuario/', views.ver_usuarios, name='ver_usuarios'),
-    path('', views.home, name='home'),
-    path('sobre-nosotros/', views.sobre_nosotros, name='sobre_nosotros'),
     path('chat/', views.chat, name='chat'),
+    path('about/', views.sobre_nosotros, name='sobre_nosotros'),
 ]
