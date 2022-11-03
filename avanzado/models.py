afdc4558fd275de5aa0affe1021d1444cc982a90
tato <tato.ocampo@live.com.ar>
2022-11-03 11:58:11 -0300

    Imagen en publicaciones

--- avanzado/models.py
+++ avanzado/models.py
@@ -8,6 +8,7 @@ class Publicacion(models.Model):
     titulo = models.CharField(max_length=20)
     subtitulo=models.CharField(max_length=20)
     linea_texto = RichTextField(max_length=400)
+    imagen = models.ImageField()
     fecha_creacion = models.DateField(null=True)
     
     def __str__(self):
