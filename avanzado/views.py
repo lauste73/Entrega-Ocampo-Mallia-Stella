bd1b2e478f71e0b3724d428950d6f4a121cddefe
tato <tato.ocampo@live.com.ar>
2022-11-03 09:59:36 -0300

    Avatares en el perfil

--- avanzado/views.py
+++ avanzado/views.py
@@ -17,7 +17,7 @@ def ver_publicaciones(request):
     if titulo:
         publicaciones = Publicacion.objects.filter(titulo__icontains=titulo)
     else:
-        publicaciones = Publicacion.objects.none()
+        publicaciones = Publicacion.objects.all()
     
     formulario = BusquedaPublicacion()
     
